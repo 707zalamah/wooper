3a460131a01253223e90941fa94fa54db455ac48
707zalamah <64750288+707zalamah@users.noreply.github.com>
2024-03-12 12:20:26 -0500

    updated wooper monitor code

--- wooper.sh
+++ wooper.sh
@@ -455,7 +455,7 @@ if [[ -d /data/data/com.gocheats.launcher ]] && [[ ! -s $exeggcute ]] ;then
 fi
 
 # enable wooper monitor
-if [[ $(grep useMonitor $wooper_versions | awk -F "=" '{ print $NF }' | awk '{ gsub(/ /,""); print }') == "true" ]] && [ -f /system/bin/wooper_monitor.sh ] ;then
+if [[ $(grep useMonitor $wooper_versions | awk -F "=" '{ print $NF }') == "true" ]] && [ -f /system/bin/wooper_monitor.sh ] ;then
   checkMonitor=$(pgrep -f /system/bin/wooper_monitor.sh)
   if [ -z $checkMonitor ] ;then
     /system/bin/wooper_monitor.sh >/dev/null 2>&1 &
