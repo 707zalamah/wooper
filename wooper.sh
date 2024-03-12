e67edfcd1b86e54c3bb7a4efeeafdf7eeb536061
707zalamah <64750288+707zalamah@users.noreply.github.com>
2024-03-12 15:40:23 -0500

    updated wooper monitor code

--- wooper.sh
+++ wooper.sh
@@ -455,14 +455,12 @@ if [[ -d /data/data/com.gocheats.launcher ]] && [[ ! -s $exeggcute ]] ;then
 fi
 
 # enable wooper monitor
-if [ -f /system/bin/wooper_monitor.sh ] ;then
   checkMonitor=$(pgrep -f /system/bin/wooper_monitor.sh)
   if [ -z $checkMonitor ] ;then
     /system/bin/wooper_monitor.sh >/dev/null 2>&1 &
     echo "`date +%Y-%m-%d_%T` wooper.sh: wooper monitor enabled" >> $logfile
     logger "wooper monitor enabled"
   fi
-fi
 
 for i in "$@" ;do
     case "$i" in
