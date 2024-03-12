eddd890abeb0895f7c8d76b15c9f8959ae6dd701
707zalamah <64750288+707zalamah@users.noreply.github.com>
2024-03-12 14:34:58 -0500

    Update initd.sh

--- initd.sh
+++ initd.sh
@@ -8,10 +8,6 @@ if [ ! -d $INITPATH ] ; then
     mkdir $INITPATH
     chmod 755 $INITPATH
     chmod 755 $INITPATH/*
-    /system/xbin/curl -s -k -L --fail --show-error -o  $INITPATH/55wooper https://raw.githubusercontent.com/707zalamah/wooper/main/55wooper
-    chmod +x $INITPATH/55wooper
-    /system/xbin/curl -s -k -L --fail --show-error -o  $INITPATH/55cron https://raw.githubusercontent.com/707zalamah/wooper/main/55cron 
-    chmod +x $INITPATH/55cron
     mount -o remount,ro /
 fi
-toybox run-parts $INITPATH
+busybox run-parts $INITPATH
